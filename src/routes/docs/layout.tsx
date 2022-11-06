0378e0f8a2caaacd8746007da75d320e514dd2f6
Timur Sultanaev <str.write@gmail.com>
2022-11-06 02:24:02 +0100

    Fix lint

--- src/routes/docs/layout.tsx
+++ src/routes/docs/layout.tsx
@@ -1,6 +1,5 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import Menu from "~/components/menu/menu";
 import OnThisPage from "~/components/on-this-page/on-this-page";
 import styles from "./docs.css";
 
