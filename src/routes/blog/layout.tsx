581682291593cdb873d68383f2e1b8dc37124087
Timur Sultanaev <str.write@gmail.com>
2023-01-22 00:37:16 +0100

    Add comments

--- src/routes/blog/layout.tsx
+++ src/routes/blog/layout.tsx
@@ -7,6 +7,24 @@ export default component$(() => {
     <>
       <Breadcrumbs></Breadcrumbs>
       <Slot />
+      <sl-divider style="--spacing: 2rem;"></sl-divider>
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="strowk/blog"
+        data-repo-id="R_kgDOIYRBFg"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOIYRBFs4CTu23"
+        data-mapping="pathname"
+        data-strict="1"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="dark_dimmed"
+        data-lang="en"
+        crossOrigin="anonymous"
+        async
+      ></script>
+      <sl-divider style="--spacing: 2rem;"></sl-divider>
       <Menu />
     </>
   );
