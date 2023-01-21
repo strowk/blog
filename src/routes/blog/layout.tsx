bad6fd262c32f2551e7fdb9a2159d6432f83c3cd
Timur Sultanaev <str.write@gmail.com>
2023-01-22 00:27:38 +0100

    add breadcrumbs

--- src/routes/blog/layout.tsx
+++ src/routes/blog/layout.tsx
@@ -1,9 +1,11 @@
 import { component$, Slot } from "@builder.io/qwik";
+import { Breadcrumbs } from "~/components/breadcrumbs/breadcrumbs";
 import Menu from "~/components/menu/menu";
 
 export default component$(() => {
   return (
     <>
+      <Breadcrumbs></Breadcrumbs>
       <Slot />
       <Menu />
     </>
